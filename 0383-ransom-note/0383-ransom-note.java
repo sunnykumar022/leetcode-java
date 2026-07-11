6954fb8bf95f5dc5aa9f3f0f89c9ecbdd6f1fab7
sunnykumar022 <sunnykumar00802@gmail.com>
2026-07-11 10:11:15 +0530

    Time: 1 ms (99.98%), Space: 46.2 MB (79.7%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int freq1[]=new int[26];
+        int freq2[]=new int[26];
+
+        for(int i=0;i<ransomNote.length();i++){
+            freq1[ransomNote.charAt(i)-'a']++;
+        }
+        for(int i=0;i<magazine.length();i++){
+            freq2[magazine.charAt(i)-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(freq1[i]>freq2[i]) return false;
+        }
+        return true;
+    }
+}
